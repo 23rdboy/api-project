f7deae10336ee2b3e3dd8a901c74f110acafd7a8
NicoletaGheorghe <g.nicole261193@gmail.com>
2025-05-19 15:41:51 +0100

    create API

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 const express = require("express");
 const app = express();
+const PORT = process.env.PORT || 3000;
+const bookRoutes = require("./routes/bookRoutes")
+
+app.use(express.json());
+
+app.use("/api/books", bookRoutes);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+});
